c024d14d04020d2ef2057c6893f483ac23cd585c
phoopyaesone <phoopyaesone984@gmail.com>
2025-07-24 21:14:33 +0630

    Create Login page and update view and URLs

--- scholarships/urls.py
+++ scholarships/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from scholarships.views import home_view, register_view
+from scholarships.views import home_view, register_view, custom_login
 from django.urls import path
 from . import views
 
@@ -12,5 +12,6 @@ app_name = 'scholarships' # This helps in namespacing URLs (e.g., 'scholarships:
 urlpatterns = [
     path('', views.scholarship_list, name='list'),
     path('register/', register_view, name='register'), # <--- ADD THIS LINE BACK HERE
+    path('login/',custom_login,name='login'),
 
 ]
