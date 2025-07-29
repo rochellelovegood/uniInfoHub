0582fed072ce8f4885b6ca90524c389a6b19bc76
phoopyaesone <phoopyaesone984@gmail.com>
2025-07-29 22:09:33 +0630

    Add homepage and design

--- scholarships/urls.py
+++ scholarships/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-<<<<<<< HEAD
+
 from scholarships.views import home_view, register_view, homepage
-=======
+
 from scholarships.views import home_view, register_view, custom_login
->>>>>>> main
+
 from django.urls import path
 from . import views
 
@@ -16,11 +16,7 @@ app_name = 'scholarships' # This helps in namespacing URLs (e.g., 'scholarships:
 urlpatterns = [
     path('', views.scholarship_list, name='list'),
     path('register/', register_view, name='register'), # <--- ADD THIS LINE BACK HERE
-<<<<<<< HEAD
     path('homepage/',views.homepage,name='homepage'),
-    path('',views.scholarship_list,name='list')
-=======
+    path('',views.scholarship_list,name='list'),
     path('login/',custom_login,name='login'),
-
->>>>>>> main
 ]
