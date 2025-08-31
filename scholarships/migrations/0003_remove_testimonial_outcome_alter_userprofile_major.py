121f239579517830df690b65421ff7110bfaee4f
Khin Than Thar <115642358+rochellelovegood@users.noreply.github.com>
2025-08-31 21:36:00 +0630

    hello

--- scholarships/migrations/0003_remove_testimonial_outcome_alter_userprofile_major.py
+++ scholarships/migrations/0003_remove_testimonial_outcome_alter_userprofile_major.py
@@ -6,8 +6,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scholarships', '0002_announcement_attachment'),
-    ]
+    ('scholarships', '0001_initial'),
+]
+
 
     operations = [
         migrations.RemoveField(
