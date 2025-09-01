8e704bec330703222568861a731b7ad427fbb6ff
Khin Than Thar <115642358+rochellelovegood@users.noreply.github.com>
2025-09-01 19:16:11 +0630

    deploy

--- uniHub/settings.py
+++ uniHub/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
